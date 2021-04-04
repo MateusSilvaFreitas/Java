642bfd6581d739b80487fb713e56d3a9b00c1006
Mateus Silva Freitas <51176161+MateusSilvaFreitas@users.noreply.github.com>
2021-04-04 18:40:15 -0300

    Create NoLista.java

---
+++ ListaComInterface/src/br/senai/NoLista.java
@@ -0,0 +1,32 @@
+package br.senai;
+
+public class NoLista<T> {
+	private T informacao;
+	private NoLista<T> next;
+	
+	
+	
+	public NoLista() {
+		this.informacao = null;
+		this.next = null;
+	}
+	public NoLista(T informacao, NoLista<T> next) {
+		this.informacao = informacao;
+		this.next = next;
+	}
+	
+	
+	public T getInformacao() {
+		return informacao;
+	}
+	public void setInformacao(T informacao) {
+		this.informacao = informacao;
+	}
+	public NoLista<T> getNext() {
+		return next;
+	}
+	public void setNext(NoLista<T> next) {
+		this.next = next;
+	}
+	
+}
